0f853bbbe851cca2f98818b1b9cf7ee2e0a4323f
Brian Vess <brian.vess@gmail.com>
2022-11-01 18:18:55 -0400

    Initial Commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tailwindConfig: './styles/tailwind.config.js',
+  "trailingComma": "all",
+  "tabWidth": 2,
+  "semi": true,
+  "singleQuote": true,
+  "printWidth": 120,
+  "bracketSpacing": true
+}
+
---
+++ .stylelintrc.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  plugins: ['stylelint-prettier'],
+  extends: ['stylelint-config-idiomatic-order', 'stylelint-config-prettier'],
+};
